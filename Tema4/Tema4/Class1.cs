7a4bc052d617b6f6a1c029875022fa2c7fa9298c
Andrei Ventuneac <andreiventuneac@hotmail.com>
2020-03-25 14:00:33 +0200

    dd

---
+++ Tema4/Tema4/Class1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teema1
+{
+    class Student
+    {
+        const int MINIM = 5;
+        const string GOOD = "admis";
+        const string BAD = "respins";
+        public const int MARE = 1;
+        public const int MIC = 0;
+
+
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public double nota { get; set; }
+        public string status { get; set; }
+        public string numecomplet { get; set; }
+
+        public Student()
+        {
+            nota = 0;
+            nume = string.Empty;
+            prenume = string.Empty;
+            status = string.Empty;
+            numecomplet = nume + ' ' + prenume;
+        }
+
+        public Student(string nume_, string prenume_, double nota_)
+        {
+            nota = nota_;
+            nume = nume_;
+            prenume = prenume_;
+            numecomplet = nume + ' ' + prenume;
+        }
+
+        public string ConversieLaSir()
+        {
+            if (nota >= 5)
+                return string.Format("Elevul {0} {1} are nota {2} si este admis", nume, prenume, nota);
+            else
+                return string.Format("Elevul {0} {1} are nota {2} si este respins", nume, prenume, nota);
+        }
+
+        public string afisareresp()
+        {
+            if (nota < 5)
+                return string.Format("{0} {1} : nota {2}", nume, prenume, nota);
+            else
+                return string.Empty;
+        }
+
+        public string afisareadmis()
+        {
+            if (nota >= 5)
+                return string.Format("{0} {1} : nota {2}", nume, prenume, nota);
+            else
+                return string.Empty;
+        }
+
+        public Student(string text)
+        {
+            int k = 0;
+            string[] cuvinte = text.Split(", ");
+            foreach (string cuv in cuvinte)
+            {
+                if (k == 0)
+                    nume = cuv;
+                if (k == 1)
+                    prenume = cuv;
+                if (k == 2)
+                    nota = Convert.ToDouble(cuv);
+                k++;
+            }
+            numecomplet = nume + ' ' + prenume;
+        }
+        public string getnumepr()
+        {
+            return string.Format("{0} {1}", nume, prenume);
+        }
+        public string reexaminare()
+        {
+            return string.Empty;
+        }
+
+        public void setnota(double nota_)
+        {
+            nota = nota_;
+        }
+
+        public void setstatus(double nota)
+        {
+            if (nota >= MINIM)
+                status = GOOD;
+            else
+                status = BAD;
+        }
+        public int compare(Student el)
+        {
+            int ok = 0;
+            if (this.nota > el.nota)
+                ok = MARE;
+            if (this.nota == el.nota)
+                ok = String.Compare(this.nume, el.nume);
+            if (this.nota < el.nota)
+                ok = MIC;
+
+            return ok;
+        }
+        public string getstatus()
+        {
+            return status;
+        }
+
+    }
+}
---
+++ Tema4/Tema4/Program.cs
@@ -0,0 +1,280 @@
+﻿//Tema1 PIU Andrei Ventuneac 3121A Calculatoare
+//Realizare meniu
+//Optiune de reexaminare
+//Optiune de creste nota
+//Optiuni afisare
+
+using System;
+
+namespace Teema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tema1 PIU Andrei Ventuneac 3121A Calculatoare\n");
+            string intrebare_;
+            string[] intrebare = new string[] { "Ce functie este utilizata pentru a afisa un text pe o line de consola?", "Ce functie este utilizata pentru a citi o linie de pe consola?", "O clasa poate avea membrii publici?" };
+            string[] raspunss = new string[] { "Console.WriteLine()", "Console.ReadLine()", "DA" };
+            //test primul elev + afisare
+            Student elev = new Student("Andrei, Tudor, 10");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("LISTA ELEVI + NOTE + situatie: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(elev.ConversieLaSir());
+
+            //sir de obiecte + functie random
+            string[] nume = new string[] { "Popescu", "Maradona", "Georgescu", "Scutescu" };
+            string[] prenume = new string[] { "Andrei", "Alexandru", "Mircea", "Stefan" };
+
+            Student[] elevi = new Student[50];
+            Random rnd = new Random();
+            for (int i = 0; i <= 10; i++)
+            {
+                string nume_ = nume[rnd.Next(0, nume.Length)];
+                string prenume_ = prenume[rnd.Next(0, prenume.Length)];
+                double nota_ = rnd.Next(1, 10);
+                elevi[i] = new Student(nume_, prenume_, nota_);
+                elevi[i].setstatus(nota_);
+                Console.WriteLine(elevi[i].ConversieLaSir());
+            }
+            //afisare elevi respinsi
+            int nr = 0;
+            Console.WriteLine("\nElevii respinsi:");
+            for (int i = 0; i <= 10; i++)
+            {
+                if (elevi[i].afisareresp() != string.Empty)
+                {
+                    nr++;
+                    Console.WriteLine(elevi[i].afisareresp());
+                }
+            }
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\nR: Reexaminare elevi");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("C: Crestere nota elevi admisi");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("A: Afisare lista elevi");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("F: Afisare lista elevi respinsi");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("O: Comparare doi elevi");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("X: Exit");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write("ALEGERE OPTIUNE: ");
+                string c = Console.ReadLine();
+                switch (c)
+                {
+                    case "R":
+                        Console.WriteLine("\nDoriti o reexaminare a elevilor? (DA/NU)\n");
+                        string raspuns;
+                        raspuns = Console.ReadLine();
+                        int admis = 0;
+
+                        if (raspuns == "DA")
+                        {
+                            for (int i = 0; i <= 10; i++)
+                            {
+                                if (elevi[i].getstatus().Equals("respins"))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("\n\n---------------------------------------------------------------------------------------------------------------");
+                                    Console.WriteLine("|                                                                                                              |");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write("|   " + string.Format(elevi[i].getnumepr() + " cu intrebarea:  "));
+                                    intrebare_ = intrebare[rnd.Next(0, intrebare.Length)];
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.Write(" " + intrebare_ + "\n");
+                                    Console.WriteLine("|                                                                                                              |");
+                                    Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+                                    Console.Write("\nRASPUNS : ");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    string raspuns_ = Console.ReadLine();
+
+                                    for (int j = 0; j <= 2; j++)
+                                    {
+                                        if (intrebare_.Equals(intrebare[j]))
+                                        {
+                                            if (raspuns_.Equals(raspunss[j]))
+                                            {
+                                                admis = 1;
+                                            }
+                                            else
+                                            {
+                                                admis = 0;
+                                            }
+                                        }
+                                    }
+
+                                    if (admis == 1)
+                                    {
+                                        Console.WriteLine("Felicitari sunteti admis cu nota 5!\n");
+                                        elevi[i].setnota(5);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Ati picat iar testul!");
+                                    }
+
+                                }
+                            }
+                        }
+
+                        //reexaminare - raspunde la o intrebare pentru a trece sau ramai picat!
+                        //reafisare elevi respinsi
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nElevii respinsi dupa reactualizare:");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        for (int i = 0; i <= 10; i++)
+                        {
+                            if (elevi[i].getstatus().Equals("respins") && elevi[i].afisareresp() != string.Empty)
+                            {
+                                Console.WriteLine(elevi[i].afisareresp());
+                            }
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "C":
+                        Console.WriteLine("\nDoriti marirea notelor elevilor admisi? (DA/NU)\n");
+                        string raspuns1;
+                        raspuns1 = Console.ReadLine();
+                        int admis1 = 0;
+                        if (raspuns1 == "DA")
+                        {
+                            for (int i = 0; i <= 10; i++)
+                            {
+                                if (elevi[i].getstatus().Equals("admis") && elevi[i].nota < 10)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("\n\n---------------------------------------------------------------------------------------------------------------");
+                                    Console.WriteLine("|                                                                                                              |");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.Write("|   " + string.Format(elevi[i].getnumepr() + " cu intrebarea:  "));
+                                    intrebare_ = intrebare[rnd.Next(0, intrebare.Length)];
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write(" " + intrebare_ + "\n");
+                                    Console.WriteLine("|                                                                                                              |");
+                                    Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+                                    Console.Write("\nRASPUNS : ");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    string raspuns_ = Console.ReadLine();
+
+                                    for (int j = 0; j <= 2; j++)
+                                    {
+                                        if (intrebare_.Equals(intrebare[j]))
+                                        {
+                                            if (raspuns_.Equals(raspunss[j]))
+                                            {
+                                                admis1 = 1;
+                                            }
+                                            else
+                                            {
+                                                admis1 = 0;
+                                            }
+                                        }
+                                    }
+
+                                    if (admis1 == 1)
+                                    {
+                                        Console.WriteLine("Felicitari ti-ai marit nota cu un punct!\n");
+                                        elevi[i].nota += 1;
+
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Raspuns gresit!");
+                                    }
+
+                                }
+                            }
+                        }
+
+
+                        //reafisare elevi admisi
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nNotele elevilor admisi dupa reactualizare: ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        for (int i = 0; i <= 10; i++)
+                        {
+                            if (elevi[i].getstatus().Equals("admis") && elevi[i].afisareadmis() != string.Empty)
+                            {
+                                Console.WriteLine(elevi[i].afisareadmis());
+                            }
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "A":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nAFISARE LISTA ELEVI");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        for (int i = 0; i <= 10; i++)
+                        {
+                            Console.WriteLine(elevi[i].ConversieLaSir());
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "F":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nElevii respinsi");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        for (int i = 0; i <= 10; i++)
+                        {
+                            if (elevi[i].getstatus().Equals("respins") && elevi[i].afisareresp() != string.Empty)
+                            {
+                                Console.WriteLine(elevi[i].afisareresp());
+                            }
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "O":
+                        Console.Write("\nCITIRE NUMAR SUDENT1: ");
+                        int nrstud1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("CITIRE NUMAR SUDENT2: ");
+                        int nrstud2 = Convert.ToInt32(Console.ReadLine());
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Ati ales studentii " + elevi[nrstud1].numecomplet + "cu nota " + elevi[nrstud1].nota + " si " + elevi[nrstud2].numecomplet + "cu nota " + elevi[nrstud2].nota);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        int ok;
+                        ok = elevi[nrstud1].compare(elevi[nrstud2]);
+
+                        if (ok == Student.MARE)
+                        {
+                            Console.WriteLine("ELEVUL " + elevi[nrstud1].numecomplet + " > ELEVUL " + elevi[nrstud2].numecomplet);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Aveam asteptari mai mari pentru tine domnule " + elevi[nrstud2].numecomplet);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                            Console.WriteLine("ELEVUL " + elevi[nrstud1].numecomplet + " < ELEVUL " + elevi[nrstud2].numecomplet);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Aveam asteptari mai mari pentru tine domnule " + elevi[nrstud1].numecomplet);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "X":
+                        Environment.Exit(0);
+                        break;
+
+                }
+
+
+            }
+
+        }
+    }
+}
